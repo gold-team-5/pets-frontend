6b7657f04cb358f4e0735c88c2330851fc2d0fda
mr-atta <mohammadatta97@gmail.com>
2021-10-14 11:00:33 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import ToDo from './component/ToDo/ToDo';
-import SettingProvider from './context';
+import React from "react";
+import ToDo from "./component/ToDo/ToDo";
+import SettingProvider from "./context";
 
 import Auth from "./component/context/auth";
 import LoginProvider from "./component/context/context";
 import Login from "./component/context/login";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./component/Header/header.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-
-
         <LoginProvider>
           <SettingProvider>
-          
             {/* user - editor - admin */}
-            
-              <ToDo />
-           
+
+            <ToDo />
           </SettingProvider>
         </LoginProvider>
       </div>
